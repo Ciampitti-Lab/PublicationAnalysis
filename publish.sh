59a302e5957adda3edaaf8fcbe8939eb90b06b63
Ivan Ramirez <locosteve4526@gmail.com>
2026-03-30 17:57:10 -0400

    file to uptade page

---
+++ publish.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+jupyter nbconvert eda_publications.ipynb \
+  --to html \
+  --output index.html \
+  --TagRemovePreprocessor.enabled=True \
+  --TagRemovePreprocessor.remove_cell_tags="['hide-cell']" \
+  --TagRemovePreprocessor.remove_input_tags="['hide-input']"
+
+git add index.html
+git add eda_publications.ipynb
+git commit -m "Update published notebook"
+git push origin Add/data-analisys-with-plotting
