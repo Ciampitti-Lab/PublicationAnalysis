5c19b1354ab4662a408467ad37ea5f1bfcf800c6
Ivan Ramirez <locosteve4526@gmail.com>
2026-04-07 15:18:40 -0400

    Adjust the github page deployment and added the structure for a multiple notebooks deployment

--- publish.sh
+++ publish.sh
@@ -3,8 +3,8 @@ jupyter nbconvert eda_publications.ipynb \
   --to html \
   --output index.html \
   --TagRemovePreprocessor.enabled=True \
-  --TagRemovePreprocessor.remove_cell_tags="['hide-cell']" \
-  --TagRemovePreprocessor.remove_input_tags="['hide-input']"
+  --TagRemovePreprocessor.remove_cell_tags hide-cell \
+  --TagRemovePreprocessor.remove_input_tags hide-input
 
 git add index.html
 git add eda_publications.ipynb
